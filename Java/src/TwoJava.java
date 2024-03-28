9258071c07ea249ec8119b17a4bb1c0b65d17b2a
Aléxia Vieira Coelho <124118325+alexiiavieirac@users.noreply.github.com>
2024-03-28 00:02:47 -0300

    Java Programming
    My first tests with Java language.

---
+++ Java/src/TwoJava.java
@@ -0,0 +1,13 @@
+public class TwoJava {
+
+    public static void main(String[] args) {
+        // write your code here
+        boolean weekend = true;
+        boolean makingSun = false;
+        // boolean goToTheBeach = weekend && makingSun;
+        // System.out.println("Are we going to the beach at the weekend? " + goToTheBeach);
+
+        String mesage = weekend ? "It's weekend" : "It's not weekend";
+        System.out.println(mesage);
+    }
+}
